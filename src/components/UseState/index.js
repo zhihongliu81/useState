42be12397e299e194e43738bdbfd4a1a93af9506
Zhihong Liu <102339574+zhihongliu81@users.noreply.github.com>
2022-07-13 11:35:55 -0500

    finished

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import "./UseState.css";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
   return (
-    <div>
+    <div className={`${theme} state`}>
       <h1>UseState Component</h1>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "light" ? "dark" : "light";
+          })
+        }
+      >
+        Change Theme
+      </button>
+      <h1>
+        {count}
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+          Decrement
+        </button>
+      </h1>
     </div>
   );
 };
